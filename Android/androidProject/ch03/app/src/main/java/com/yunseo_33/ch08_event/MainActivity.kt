1d5971a1e7b195102db95f4ee8d5723c18418546
BangYunseo <yunseobang33@gmail.com>
2025-04-11 13:52:14 +0900

    modify

--- Android/androidProject/ch03/app/src/main/java/com/yunseo_33/ch08_event/MainActivity.kt
+++ Android/androidProject/ch03/app/src/main/java/com/yunseo_33/ch08_event/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.yunseo_33.ch08_event
 
 import android.os.Bundle
+import android.view.MotionEvent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.util.Log
+import android.view.KeyEvent
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean{
+        when(keyCode){
+            KeyEvent.KEYCODE_0 -> Log.d("Yunseo", "0 키가 눌림")
+            KeyEvent.KEYCODE_A -> Log.d("Yunseo", "A 키가 눌림")
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
